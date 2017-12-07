55b21eae1cf45de48c7073dd7571d731a3cedd63
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-12-07 16:26:20 -0200

    add most of the useful autoproj build targets

--- src/tasks.ts
+++ src/tasks.ts
@@ -27,16 +27,66 @@ export class Provider implements vscode.TaskProvider
         })
     }
 
-    private createBuildTask(name, ws, folder = null) {
-        let definition = { type: 'autoproj', kind: 'build', workspace: ws.root, folder: folder }
-        let extra_args = []
-        if (folder) {
-            extra_args.push(folder);
-        }
-        let exec = this.runAutoproj(ws, 'build', '--tool', ...extra_args);
+    private createTask(name, group, ws, defs = {}, args = []) {
+        let definition = { type: 'autoproj', workspace: ws.root, ...defs }
+        let exec = this.runAutoproj(ws, ...args);
         let task = new vscode.Task(definition, name, 'autoproj', exec, []);
-        task.group = vscode.TaskGroup.Build;
+        task.group = group;
         return task;
+    }
+
+    private createOsdepsTask(name, ws, defs = {}, args = []) {
+        return this.createTask(name, vscode.TaskGroup.Build, ws,
+            { mode: 'osdeps', ...defs },
+            ['osdeps', '--color', ...args]);
+    }
+
+    private createBuildTask(name, ws, defs = {}, args = []) {
+        return this.createTask(name, vscode.TaskGroup.Build, ws,
+            { mode: 'build', ...defs },
+            ['build', '--tool', ...args]);
+    }
+
+    private createUpdateTask(name, ws, defs = {}, args = []) {
+        return this.createTask(name, vscode.TaskGroup.Build, ws,
+            { mode: 'update', ...defs },
+            ['update', '--progress=f', '-k', '--color', ...args]);
+    }
+
+    private createUpdateConfigTask(name, ws, defs = {}, args = []) {
+        return this.createUpdateTask(name, ws,
+            defs,
+            [ '--config', ...args]);
+    }
+
+    private createCheckoutTask(name, ws, defs = {}, args = []) {
+        return this.createUpdateTask(name, ws,
+            defs,
+            ['--checkout-only', ...args]);
+    }
+
+    private createPackageBuildTask(name, ws, folder, defs = {}, args = []) {
+        return this.createBuildTask(name, ws,
+            { folder: folder, ...defs },
+            [folder, ...args])
+    }
+
+    private createPackageForceBuildTask(name, ws, folder, defs = {}, args = []) {
+        return this.createPackageBuildTask(name, ws, folder,
+            defs,
+            ['--force', '--deps=f', '--no-confirm', ...args]);
+    }
+
+    private createPackageUpdateTask(name, ws, folder, defs = {}, args = []) {
+        return this.createUpdateTask(name, ws,
+            { folder: folder, ...defs },
+            [folder, ...args]);
+    }
+
+    private createPackageCheckoutTask(name, ws, folder, defs = {}, args = []) {
+        return this.createPackageUpdateTask(name, ws,
+            defs,
+            ['--checkout-only', ...args]);
     }
 
     provideTasks(token)
@@ -44,10 +94,18 @@ export class Provider implements vscode.TaskProvider
         let result = [];
         this.workspaces.forEachWorkspace((ws) => {
             result.push(this.createBuildTask(`${ws.name}: Build`, ws));
+            result.push(this.createCheckoutTask(`${ws.name}: Checkout`, ws));
+            result.push(this.createOsdepsTask(`${ws.name}: Install OS Dependencies`, ws));
+            result.push(this.createUpdateConfigTask(`${ws.name}: Update Configuration`, ws));
+            result.push(this.createUpdateTask(`${ws.name}: Update`, ws));
         })
         this.workspaces.forEachFolder((ws, folder) => {
+            if (folder == ws.root) { return; }
             let relative = path.relative(ws.root, folder);
-            result.push(this.createBuildTask(`${ws.name}: Build ${relative}`, ws, folder));
+            result.push(this.createPackageBuildTask(`${ws.name}: Build ${relative}`, ws, folder));
+            result.push(this.createPackageCheckoutTask(`${ws.name}: Checkout ${relative}`, ws, folder));
+            result.push(this.createPackageForceBuildTask(`${ws.name}: Force Build ${relative}`, ws, folder));
+            result.push(this.createPackageUpdateTask(`${ws.name}: Update ${relative}`, ws, folder));
         })
         return result;
     }
