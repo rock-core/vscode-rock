03d9ebd00f8f48cf70a891db131a6a20b1a6dbb0
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-12-07 16:24:26 -0200

    clean preamble of test files
    This was still the preamble generated by the scaffold generator.

--- src/test/autoproj.test.ts
+++ src/test/autoproj.test.ts
@@ -1,21 +1,11 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
+'use strict';
 import * as assert from 'assert';
 import * as fs from 'fs';
 import * as path from 'path';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as autoproj from '../autoproj';
-
 import * as helpers from './helpers';
 
-// Defines a Mocha test suite to group tests of similar kind together
 describe("Autoproj helpers tests", function () {
     let root: string;
     beforeEach(function () {
