d249eb6bfe136ec91c83797cb5538096e8453cfa
Sylvain Joyeux <sylvain.joyeux@13robotics.com>
2019-02-28 11:49:53 -0300

    attempt to update obsolete / unmaintained dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,8 @@ var path = require('path');
 var runSequence = require('run-sequence');
 var del = require('del');
 var jeditor = require("gulp-json-editor");
-var coveralls = require('gulp-coveralls');
 
 gulp.task('clean', function(callback) {
-    runSequence('cover:clean', 'out:clean', callback);
-});
-
-gulp.task('cover:clean', function (done) {
-    return del('coverage', done);
-});
-
-gulp.task('cover:enable',() => {
-    return gulp.src("./coverconfig.json")
-        .pipe(jeditor(function(json) {
-            json.enabled = true;
-            return json; // must return JSON object.
-        }))
-        .pipe(gulp.dest("./", {'overwrite':true}));
-});
-
-gulp.task('cover:disable', () => {
-    return gulp.src("./coverconfig.json")
-        .pipe(jeditor(function(json) {
-            json.enabled = false;
-            return json; // must return JSON object.
-        }))
-        .pipe(gulp.dest("./", {'overwrite':true}));
-});
-
-gulp.task('cover:publish', function(callback) {
-    return gulp.src('coverage/lcov.info')
-        .pipe(coveralls());
 });
 
 gulp.task('out:clean', function(callback) {
