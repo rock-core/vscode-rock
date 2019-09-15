010fbf7d729886c996972435d01a12feb478ca38
Gabriel Arjones <gabriel.arjones@tidewise.io>
2019-09-15 15:24:53 -0300

    re-enable code coverage on travis

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,29 @@ var del = require('del');
 var jeditor = require("gulp-json-editor");
 
 gulp.task('clean', function(callback) {
+    runSequence('cover:clean', 'out:clean', callback);
+});
+
+gulp.task('cover:clean', function (done) {
+    return del('coverage', done);
+});
+
+gulp.task('cover:enable',() => {
+    return gulp.src("./coverconfig.json")
+        .pipe(jeditor(function(json) {
+            json.enabled = true;
+            return json; // must return JSON object.
+        }))
+        .pipe(gulp.dest("./", {'overwrite':true}));
+});
+
+gulp.task('cover:disable', () => {
+    return gulp.src("./coverconfig.json")
+        .pipe(jeditor(function(json) {
+            json.enabled = false;
+            return json; // must return JSON object.
+        }))
+        .pipe(gulp.dest("./", {'overwrite':true}));
 });
 
 gulp.task('out:clean', function(callback) {
